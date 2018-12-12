a8a2d6e967fd8fa37b15a9f66e19a1b0676c8532
Mx <137980065@qq.com>
2018-12-11 19:01:17 -0800

    123

---
+++ pvp/Assets/scripts/CommonStatus.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommonStatus : MonoBehaviour
+{
+	public Slider HPStrip;
+	public float Atk;
+	public int Combo;
+	public float hp;
+
+	private BattleManager battleManager;
+	private SoundManager soundManager;
+
+	void Awake ()
+	{
+		//battleManager = GameObject.Find ("BattleManager").GetComponent<BattleManager> ();
+		//soundManager = GetComponent<SoundManager> ();
+	}
+
+	public void HealthDmg (float damage, float multiplier)
+	{
+		if (hp > 0) {
+			damage += damage * Combo / 100;
+			hp -= damage * multiplier;
+			HPStrip.value = hp;
+			damage /= multiplier;
+			damage -= damage * Combo / 100;
+		}
+	}
+
+	public void HPRecover ()
+	{
+		if (hp <= HPStrip.maxValue) {
+			hp += 20 * Time.deltaTime;
+			HPStrip.value = hp;
+		}
+
+	}
+}
---
+++ pvp/Assets/scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public IEnumerator LoadAudio (string name, float Volume, float Pitch, float time, float delay, bool isloop)
+	{
+		string url = @"C:\Users\Shintarou\Desktop\Sound\" + name;
+		WWW www = new WWW ("file://" + url);
+		yield return www;
+		AudioClip clip = www.GetAudioClip ();
+		GameObject audio = new GameObject ("Audio");
+		AudioSource source = audio.AddComponent<AudioSource> ();
+		source.loop = isloop;
+		source.clip = clip;
+		source.volume = Volume;
+		source.pitch = Pitch;
+		yield return new WaitForSeconds (delay);
+		source.Play ();	
+		yield return new WaitForSeconds (time);
+		Destroy (audio);
+	}
+}
